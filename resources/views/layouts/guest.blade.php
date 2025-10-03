eda0bab4d92f1fa13c684e4598369bfb9f0a91b7
Rick van der Wolk <rickvanderwolk@gmail.com>
2025-10-03 18:45:12 +0200

    Fix favicon + iOS status bar color + app name in webmanifest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -2,17 +2,21 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+        <meta name="theme-color" content="#0A0A0A" media="(prefers-color-scheme: dark)">
+        <meta name="theme-color" content="#FFFEF9" media="(prefers-color-scheme: light)">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'happynotes') }}</title>
         <meta name="description" content="💥🧠📝🎨🚀">
 
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-title" content="happynotes" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png?v=2" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2" />
+        <link rel="shortcut icon" href="/favicon.ico?v=2" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2" />
+        <meta name="apple-mobile-web-app-title" content="HappyNotes" />
         <link rel="manifest" href="/site.webmanifest" />
 
         <!-- Styles -->
